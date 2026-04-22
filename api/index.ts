9bc1802d5209a224114d7d9a22fe36833dc27ac8
luvia <luviaamagaali07@gmail.com>
2026-04-21 23:18:47 -0600

    Corregir configuración de Vercel para servir rutas correctamente

--- api/index.ts
+++
@@ -1,7 +0,0 @@
-// Entry point para Vercel Serverless Functions
-// Vercel requiere que el handler esté en /api/
-import 'dotenv/config'
-import app from '../src/app'
-
-// Exportar la app de Express como handler de Vercel
-export default app
